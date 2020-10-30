953465aa3ce47689814b252200a31a0994b9513e
dconwaylee <60713510+dconwaylee@users.noreply.github.com>
2020-10-30 15:34:48 +0000

    StringBuilder in Java.  Going through all different components of the String function.

---
+++ Java-Week2/src/Week2/StringBuilder.java
@@ -0,0 +1,36 @@
+package Week2;
+
+public class StringBuilder {
+//baldpate
+//ablated
+
+   public static void main(String[] args) {
+
+      //Dom
+      String phrase = "adaptable";
+      String part1 = phrase.substring(0,1);
+      String part2 = phrase.substring(1,2);
+      String part3 = phrase.substring(3,4);
+      String part4 = phrase.substring(4,5);
+      //System.out.print(part1+part2+part1+part3+part4);
+      //Adapt
+
+      String word = "MGJACOLIPVINA";
+      String ltr1 = word.substring(3,4);
+      String ltr2 = word.toLowerCase().substring(1,2);
+      String ltr3 = word.toLowerCase().substring(6,7);
+      String ltr4 = word.toLowerCase().substring(7,8);
+      String ltr5 = word.toLowerCase().substring(3,4);
+      String ltr6 = word.toLowerCase().substring(11,12);
+      String ltr7 = word.toLowerCase().substring(4,5);
+      String ltr8 = word.toLowerCase().substring(5,6);
+
+      System.out.print(ltr1+ltr2+ltr3+ltr4+ltr5+ltr6+ltr4+ltr7+ltr8);
+      //Aglianico
+
+
+
+
+   }
+
+}

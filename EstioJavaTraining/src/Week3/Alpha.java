f16eb21c37357b520f216881af824212ea4e3427
dconwaylee <60713510+dconwaylee@users.noreply.github.com>
2020-11-09 15:28:46 +0000

    Day 11 - Good session StringBuilder was the main topic

---
+++ EstioJavaTraining/src/Week3/Alpha.java
@@ -0,0 +1,15 @@
+package Week3;
+
+public class Alpha {
+
+   public static void main(String[] args) {
+
+
+      String alpha = "";
+      for (char current = 'a'; current <= 'z'; current++) {
+         alpha += current;
+      }
+      System.out.println(alpha);
+
+   }
+}

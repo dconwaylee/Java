a04368171f47daa27040103b4224bea4c8ae9d1a
dconwaylee <60713510+dconwaylee@users.noreply.github.com>
2020-11-10 15:39:01 +0000

    End of day commit

--- EstioJavaTraining/src/Week3/MultiDimensionArray.java
+++ EstioJavaTraining/src/Week3/MultiDimensionArray.java
@@ -1,5 +1,7 @@
 package Week3;
 
+import java.util.ArrayList;
+
 public class MultiDimensionArray {
 
    public static void main(String[] args) {
@@ -16,5 +18,28 @@ public class MultiDimensionArray {
          System.out.println("-");
       }
       System.out.println("------------");
+
+
+      //Enhanced loop to do the array
+      //this is far more elegant & certainly saves
+      //on white space & code overhead.
+      for (int[] inner : multiD) {
+         for (int num : inner) System.out.print(num + " ");
+         System.out.println();
+      }
+
+      ArrayList<String> FastNFurious = new ArrayList<>();
+      FastNFurious.add("Brian");
+      FastNFurious.add("Dom");
+      FastNFurious.add("Letty");
+      FastNFurious.add("Tim");
+
+
+      System.out.println(FastNFurious.remove(2) + " Looses her memory, removed from the team!");
+      System.out.println(FastNFurious);
+      System.out.println(FastNFurious.set(3, "Jessie") + "'s name was wrong on the script");
+      System.out.println(FastNFurious.size());
+      //System.out.println(FastNFurious.isEmpty()); // This is causing me errors
+
    }
 }
